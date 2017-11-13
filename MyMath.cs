8183d633616a55266320526f48277987b382fd98
s01116196 <33640790+s01116196@users.noreply.github.com>
2017-11-13 17:42:24 -0500

    Initial commit

---
+++ MyMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class MyMath
+    {
+        double result;
+        double operand1;
+        double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+
+        }
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+        }
+        public void Subtract(double operand1, double operand2)
+        { result = operand1 - operand2; }
+
+        public void Add(double operand1, double operand2)
+        { result = operand1 + operand2; }
+        public double GetResult()
+
+        { return result;}
+
+
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Program
+    {
+        public static void Main(string []args)
+        {
+            MyMath calc = new MyMath();
+            calc.Add(3.45, 4.29);
+            Console.WriteLine("Add: " + calc.GetResult());
+
+            
+            calc.Subtract(9.75, 4.29);
+            Console.WriteLine("Subtract: " + calc.GetResult());
+
+     
+            calc.Divide(200, 4.5);
+            Console.WriteLine("Divide: " + calc.GetResult());
+
+       
+            calc.Multiply(22.6, 76.5);
+            Console.WriteLine("Multiply: " + calc.GetResult());
+
+
+        }
+    }
+}
